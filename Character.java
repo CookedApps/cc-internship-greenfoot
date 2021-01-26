d4fcbff6e9fb723978a817db5f125c415505679a
Viking180 <38494132+Viking180@users.noreply.github.com>
2021-01-26 11:58:14 +0100

    add character.java with method declaration && sans.png

---
+++ Character.java
@@ -0,0 +1,28 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Character here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Character extends Actor
+{
+    /**
+     * Act - do whatever the Character wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }   
+    public void walk(){
+        System.out.println("here well walk");
+    }
+    public void talkWith(){
+        System.out.println("here well have the talk with");
+    }
+    public void readMessage(){
+        System.out.println("here we'll read the messages");
+    }
+}
