bc53cc5b8dc0e374d06f9f62cad42b4f93e7340e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-26 12:08:20 +0100

    messageboard added

---
+++ messageBoard.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class messageBoard here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class messageBoard extends Actor
+{
+    public void messageBoard(){
+            setImage("images/msgboard.png");
+    }
+    /**
+     * Act - do whatever the messageBoard wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+       
+    }  
+    public void printOut(){
+        
+    }
+    
+}
