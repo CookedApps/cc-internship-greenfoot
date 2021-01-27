b5109d6459400139b98a4378228a927b73720642
Viking180 <38494132+Viking180@users.noreply.github.com>
2021-01-27 23:34:31 +0100

    add wall && press h for character +1 homework (im mooment ein nullpointer)

--- MyWorld.java
+++ MyWorld.java
@@ -11,6 +11,7 @@ public class MyWorld extends World
     public static messageBoard mb1;
     public static soundPlayer spr;
     public static Friends frnd1;
+    public static Wall wall;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -31,5 +32,7 @@ public class MyWorld extends World
         addObject(spr, 1140, 24);
         Friends frnd1 = new Friends();
         addObject(frnd1, 100, 700);
+        Wall wall = new Wall();
+        addObject(wall, 800, 400);
     }
 }
