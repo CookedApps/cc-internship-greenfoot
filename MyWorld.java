3f8f8e7ae3a1d6246499d8eafa2467c29ed54112
Viking180 <38494132+Viking180@users.noreply.github.com>
2021-01-28 11:10:31 +0100

    add TrumpMethod for creating walls add setWallsSubline to build walls on subline

--- MyWorld.java
+++ MyWorld.java
@@ -12,6 +12,9 @@ public class MyWorld extends World
     public static soundPlayer spr;
     public static Friends frnd1;
     public static Wall wall;
+    Wall[] wallarr = new Wall[20];
+    int posx = 1140;
+    int posy = 786;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -32,7 +35,42 @@ public class MyWorld extends World
         addObject(spr, 1140, 24);
         Friends frnd1 = new Friends();
         addObject(frnd1, 100, 700);
-        Wall wall = new Wall();
-        addObject(wall, 800, 400);
+        //walls -----------------------------------------------
+        setWallsSubline();
     }
+    public void trumpMethod(){
+        Wall wall = new Wall();
+        Wall wall1 = new Wall();
+        Wall wall2 = new Wall();
+        Wall wall3 = new Wall();
+        Wall wall4 = new Wall();
+        Wall wall5 = new Wall();
+        Wall wall6 = new Wall();
+        Wall wall7 = new Wall();
+        Wall wall8 = new Wall();
+        Wall wall9 = new Wall();
+        Wall wall10 = new Wall();
+        
+        wallarr[0] = wall;
+        wallarr[1] = wall1;
+        wallarr[2] = wall2;
+        wallarr[3] = wall3;
+        wallarr[4] = wall4;
+        wallarr[5] = wall5;
+        wallarr[6] = wall6;
+        wallarr[7] = wall7;
+        wallarr[8] = wall8;
+        wallarr[9] = wall9;
+        wallarr[10] = wall10;
+    }
+    public void setWallsSubline(){
+        trumpMethod();
+        
+        
+        for(int i =0; i < 10; i++){
+            addObject(wallarr[i], posx, posy);
+            posx = posx - 120;
+        }
+    }
+    
 }
